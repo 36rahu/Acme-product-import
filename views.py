7427f27750a8566053a31e98cbb798e96e505482
rahul-parambil <rahul.parambil@tarams.com>
2019-08-31 01:13:45 +0530

    new features

--- views.py
+++ views.py
@@ -10,12 +10,14 @@ from celery_task import insert_value_in_model
 import pandas as pd
 import numpy as np
 
-from constants import PER_PAGE
+from constants import EDIT_LINK
 from utils import allowed_file
 from models import Products
 from products_management import add_product
+from webhook_manager import WebhookManager
 
 views = Blueprint('views', __name__)
+webhook_manager = WebhookManager()
 
 @views.route('/')
 def upload_form():
@@ -30,21 +32,26 @@ def upload_file():
         file = request.files['file']
         if file.filename == '':
             flash('No file selected for uploading')
-            return redirect(request.url)
+            return redirect('/')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             flash('File successfully uploaded')
+            # Celery task to send the relatime status
             task = insert_value_in_model.apply_async(args=[file_path])
             return redirect('/')
         else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-            return redirect(request.url)
+            flash('Only CSV file allowed.')
+            return redirect('/')
 
 @views.route('/products_list/')
 def get_products_list():
-    products = [{'sku': prod.sku, 'name': prod.name, 'description': prod.description, 'status': prod.status.name} 
+    products = [{'sku': prod.sku,
+                'name': prod.name,
+                'description': prod.description,
+                'status': prod.status.name,
+                'edit_link': EDIT_LINK.format(prod.sku)} 
     for prod in db.session.query(Products).all()]
     return jsonify({'data': products})
 
@@ -64,11 +71,34 @@ def delete_products():
             flash('Products deletion failed')
     return render_template('products_delete.html')
 
-
 @views.route('/products/add/', methods=['POST', 'GET'])
 def add_products():
     if request.method == 'POST':
         res = add_product(request.form)
         flash(res.get('msg'))
     return render_template('product_add.html')
+
+@views.route('/product/edit/<sku>/', methods=['POST', 'GET'])
+def edit_products(sku=None):
+    if request.method == 'POST':
+        res = add_product(request.form)
+        flash(res.get('msg'))
+    product = Products.query.filter(Products.sku == sku).first()
+    if product:
+        product_data = {'sku': product.sku,
+                        'name': product.name,
+                        'description': product.description,
+                        'status': product.status.name
+                        }
+        return render_template('product_edit.html', data=product_data)
+    flash('Products not exit')
+    return render_template('product_edit.html')
+
+@views.route('/webhook/add/', methods=['POST', 'GET'])
+def add_webhook():
+    if request.method == 'POST':
+        webhook_manager.create_webhook(request.form)
+        flash('webhook creeated')
+    return render_template('webhook_add.html')
+
 
